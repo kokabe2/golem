b6aae0eff948db373e66dbc89cbe9ae6d4845c21
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:44:14 +0900

    Add PhotosensorFactory interface

---
+++ v1/photosensor_factory_interface.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_PHOTOSENSOR_FACTORY_INTERFACE_H_
+#define V1_PHOTOSENSOR_FACTORY_INTERFACE_H_
+
+#include "photosensor_interface.h"
+
+typedef struct {
+  PhotosensorInterface (*Make)(int id);
+} PhotosensorFactoryInterfaceStruct;
+typedef const PhotosensorFactoryInterfaceStruct* PhotosensorFactoryInterface;
+
+#endif  // V1_PHOTOSENSOR_FACTORY_INTERFACE_H_
