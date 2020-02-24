0c9c638a28388e13143cc221250e27cfb590dbc1
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:37:41 +0900

    Add NullMicroswitch class

---
+++ v1/null_microswitch.h
@@ -0,0 +1,10 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_NULL_MICROSWITCH_H_
+#define V1_NULL_MICROSWITCH_H_
+
+#include "microswitch_interface.h"
+
+extern const MicroswitchInterface nullMicroswitch;
+
+#endif  // V1_NULL_MICROSWITCH_H_
