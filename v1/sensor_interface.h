16a553e41a3ccb17ae3f5029313fdde9de63117d
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:14:52 +0900

    Add Sensor interface

---
+++ v1/sensor_interface.h
@@ -0,0 +1,16 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SENSOR_INTERFACE_H_
+#define V1_SENSOR_INTERFACE_H_
+
+#include <stdbool.h>
+
+typedef struct {
+  int (*Id)(void);
+  const char* (*Tag)(void);
+  const char* (*State)(void);
+  bool (*IsDefaultState)(void);
+} SensorInterfaceStruct;
+typedef const SensorInterfaceStruct* SensorInterface;
+
+#endif  // V1_SENSOR_INTERFACE_H_
