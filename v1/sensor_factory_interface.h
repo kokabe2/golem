199c4a8c97e7feef212146bccdb6acc4a622f1d6
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:16:50 +0900

    Add SensorFactory interface

---
+++ v1/sensor_factory_interface.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SENSOR_FACTORY_INTERFACE_H_
+#define V1_SENSOR_FACTORY_INTERFACE_H_
+
+#include "sensor_interface.h"
+
+typedef struct {
+  SensorInterface (*Make)(int id);
+} SensorFactoryInterfaceStruct;
+typedef const SensorFactoryInterfaceStruct* SensorFactoryInterface;
+
+#endif  // V1_SENSOR_FACTORY_INTERFACE_H_
