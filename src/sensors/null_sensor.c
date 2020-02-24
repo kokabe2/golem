bbc1edfb2590f49ef1ba3a9f00e06017b2c6e36b
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:24:17 +0900

    Add NullSensor class

---
+++ src/sensors/null_sensor.c
@@ -0,0 +1,12 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "null_sensor.h"
+
+static int Id(void) { return ~0; }
+static const char* Tag(void) { return ""; }
+static const char* State(void) { return ""; }
+static bool IsDefaultState(void) { return false; }
+static const SensorInterfaceStruct kTheMethod = {
+    .Id = Id, .Tag = Tag, .State = State, .IsDefaultState = IsDefaultState,
+};
+const SensorInterface nullSensor = &kTheMethod;
