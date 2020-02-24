d58eea5f98edaf4b79cc4307a472cad29a7f3f4f
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 18:05:37 +0900

    Add NullActuator class

---
+++ src/actuators/null_actuator_test.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "null_actuator.h"
+}
+
+TEST(NullActuatorTest, NullObject) {
+  EXPECT_EQ(~0, nullActuator->Id());
+  EXPECT_STREQ("", nullActuator->Tag());
+  EXPECT_STREQ("", nullActuator->State());
+
+  EXPECT_FALSE(nullActuator->IsActive());
+  nullActuator->Activate();
+  EXPECT_FALSE(nullActuator->IsActive());
+  nullActuator->Deactivate();
+  EXPECT_FALSE(nullActuator->IsActive());
+}
