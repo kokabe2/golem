361c2f587d30d2a57457ca6b346efb8fd7f6ddc8
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 17:58:03 +0900

    Add Actuator interface

---
+++ v1/actuator_interface.h
@@ -0,0 +1,18 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_ACTUATOR_INTERFACE_H_
+#define V1_ACTUATOR_INTERFACE_H_
+
+#include <stdbool.h>
+
+typedef struct {
+  int (*Id)(void);
+  const char* (*Tag)(void);
+  const char* (*State)(void);
+  void (*Activate)(void);
+  void (*Deactivate)(void);
+  bool (*IsActive)(void);
+} ActuatorInterfaceStruct;
+typedef const ActuatorInterfaceStruct* ActuatorInterface;
+
+#endif  // V1_ACTUATOR_INTERFACE_H_
