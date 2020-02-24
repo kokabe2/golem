6a18d0d4b30a680230deca967faf0017a2e5b1bd
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:43:22 +0900

    Add Photosensor interface

---
+++ v1/photosensor_interface.h
@@ -0,0 +1,17 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_PHOTOSENSOR_INTERFACE_H_
+#define V1_PHOTOSENSOR_INTERFACE_H_
+
+#include <stdbool.h>
+
+typedef struct {
+  int (*Id)(void);
+  const char* (*Tag)(void);
+  const char* (*State)(void);
+  bool (*IsLight)(void);
+  bool (*IsDark)(void);
+} PhotosensorInterfaceStruct;
+typedef const PhotosensorInterfaceStruct* PhotosensorInterface;
+
+#endif  // V1_PHOTOSENSOR_INTERFACE_H_
