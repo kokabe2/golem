1253de38b6b7adae58a3e63094ff4c1d2d9f5fbb
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:47:41 +0900

    Add NullPhotosensor class

---
+++ src/sensors/photosensors/null_photosensor.c
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "null_photosensor.h"
+
+static int Id(void) { return ~0; }
+static const char* Tag(void) { return ""; }
+static const char* State(void) { return ""; }
+static bool IsLight(void) { return false; }
+static bool IsDark(void) { return false; }
+static const PhotosensorInterfaceStruct kTheMethod = {
+    .Id = Id, .Tag = Tag, .State = State, .IsLight = IsLight, .IsDark = IsDark,
+};
+const PhotosensorInterface nullPhotosensor = &kTheMethod;
---
+++ v1/null_photosensor.h
@@ -0,0 +1,10 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_NULL_PHOTOSENSOR_H_
+#define V1_NULL_PHOTOSENSOR_H_
+
+#include "photosensor_interface.h"
+
+extern const PhotosensorInterface nullPhotosensor;
+
+#endif  // V1_NULL_PHOTOSENSOR_H_
