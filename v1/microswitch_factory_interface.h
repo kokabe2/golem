2cbf95114a53df3b604b76c6bbaf5b2f3d59ac70
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-02-24 16:31:42 +0900

    Add MicroswitchFactory interface

---
+++ v1/microswitch_factory_interface.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_MICROSWITCH_FACTORY_INTERFACE_H_
+#define V1_MICROSWITCH_FACTORY_INTERFACE_H_
+
+#include "microswitch_interface.h"
+
+typedef struct {
+  MicroswitchInterface (*Make)(int id);
+} MicroswitchFactoryInterfaceStruct;
+typedef const MicroswitchFactoryInterfaceStruct* MicroswitchFactoryInterface;
+
+#endif  // V1_MICROSWITCH_FACTORY_INTERFACE_H_
