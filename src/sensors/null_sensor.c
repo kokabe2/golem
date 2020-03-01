3255ecc8dfaa326848a04df71d4abc1868d5e5f5
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-01 10:33:22 +0900

    Improve readability of NullSensor

--- src/sensors/null_sensor.c
+++ src/sensors/null_sensor.c
@@ -2,16 +2,14 @@
 // This software is released under the MIT License, see LICENSE.
 #include "null_sensor.h"
 
-static int Id(void) { return ~0; }
+static int NonsenseValue(void) { return ~0; }
 
-static const char* Tag(void) { return ""; }
+static const char* EmptyString(void) { return ""; }
 
-static const char* State(void) { return ""; }
-
-static bool IsDefaultState(void) { return false; }
+static bool False(void) { return false; }
 
 static const SensorInterfaceStruct kTheMethod = {
-    .Id = Id, .Tag = Tag, .State = State, .IsDefaultState = IsDefaultState,
+    .Id = NonsenseValue, .Tag = EmptyString, .State = EmptyString, .IsDefaultState = False,
 };
 
 const SensorInterface nullSensor = &kTheMethod;
