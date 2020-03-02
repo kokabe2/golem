eb64bb758d11f88c4c9f4e5a5fad218e0f701223
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-02 22:44:51 +0900

    Add options for compile

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ cd %~dp0
 set COMPILER=ccrx
 set OUTPUT_DIR=bin\%COMPILER%@%PROJECT_VER%
 mkdir %OUTPUT_DIR% > NUL 2>&1
-%COMPILER% -output=obj -lang=c99 -nostuff -isa=rxv3 ^
+%COMPILER% -output=obj -lang=c99 -nostuff -isa=rxv3 -debug -signed_char ^
            -include=v1/,external/ ^
            src/actuators/motors/dc_motors/null_dc_motor.c ^
            src/actuators/motors/pulse_motors/null_pulse_motor.c ^
