7ff160e6422849ef3a1c1a4c440ecce867ad18b1
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-02 22:44:14 +0900

    Fix build.bat

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ cd %~dp0
 set COMPILER=ccrx
 set OUTPUT_DIR=bin\%COMPILER%@%PROJECT_VER%
 mkdir %OUTPUT_DIR% > NUL 2>&1
-%COMPILER% -output=obj=%OUTPUT_DIR%/%PROJECT_NAME%.obj -lang=c99 -nostuff -isa=rxv3 ^
+%COMPILER% -output=obj -lang=c99 -nostuff -isa=rxv3 ^
            -include=v1/,external/ ^
            src/actuators/motors/dc_motors/null_dc_motor.c ^
            src/actuators/motors/pulse_motors/null_pulse_motor.c ^
@@ -24,5 +24,6 @@ mkdir %OUTPUT_DIR% > NUL 2>&1
            src/util/watch_command.c ^
            src/golem.c
 if %errorlevel% neq 0 exit /b
-rlink -form=lib -output=%OUTPUT_DIR%/%PROJECT_NAME%.lib ^
-      %OUTPUT_DIR%/%PROJECT_NAME%.obj
+
+rlink -form=lib -output=%OUTPUT_DIR%/%PROJECT_NAME%.lib *.obj
+del *.obj
