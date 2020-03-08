6e768934a60a6efe69c8690f65a17249ad0725a5
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-08 11:48:56 +0900

    Add NullLed class

---
+++ src/displays/leds/null_led.c
@@ -0,0 +1,22 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "null_led.h"
+
+static int NonsenseValue(void) { return ~0; }
+
+static const char* EmptyString(void) { return ""; }
+
+static void NoEffect(void) {}
+
+static bool False(void) { return false; }
+
+static const LedInterfaceStruct kTheMethod = {
+    .Id = NonsenseValue,
+    .Tag = EmptyString,
+    .TurnOff = NoEffect,
+    .TurnOn = NoEffect,
+    .IsOff = False,
+    .IsOn = False,
+};
+
+const LedInterface nullLed = &kTheMethod;
