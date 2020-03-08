8e2bf7b20e2978173c39f507f862d3a2cd7ee6f8
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-08 11:42:26 +0900

    Add Led interface

---
+++ v1/led_interface.h
@@ -0,0 +1,18 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_LED_INTERFACE_H_
+#define V1_LED_INTERFACE_H_
+
+#include <stdbool.h>
+
+typedef struct {
+  int (*Id)(void);
+  const char* (*Tag)(void);
+  void (*TurnOff)(void);
+  void (*TurnOn)(void);
+  bool (*IsOff)(void);
+  bool (*IsOn)(void);
+} LedInterfaceStruct;
+typedef const LedInterfaceStruct* LedInterface;
+
+#endif  // V1_LED_INTERFACE_H_
