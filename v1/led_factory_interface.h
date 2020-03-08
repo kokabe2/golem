12cf9bff6f40eae3f81f3d3bdf1dc7f5687d721b
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-08 11:43:05 +0900

    Add LedFactory interface

---
+++ v1/led_factory_interface.h
@@ -0,0 +1,13 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_LED_FACTORY_INTERFACE_H_
+#define V1_LED_FACTORY_INTERFACE_H_
+
+#include "led_interface.h"
+
+typedef struct {
+  LedInterface (*Make)(int id);  //
+} LedFactoryInterfaceStruct;
+typedef const LedFactoryInterfaceStruct* LedFactoryInterface;
+
+#endif  // V1_LED_FACTORY_INTERFACE_H_
