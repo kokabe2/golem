c1bf94ef70fba875ebaea3045d41f478595290ab
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-08 14:38:56 +0900

    Add DummyBlockCommand module

---
+++ src/util/dummy_block_command.c
@@ -0,0 +1,11 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "block_command.h"
+
+static void Do(int microseconds) {}
+
+static const BlockCommandMethodStruct kTheMethod = {
+    .Do = Do,
+};
+
+const BlockCommandMethod blockCommand = &kTheMethod;
