911a447501765eeafe523d216a275d1c408ab9bc
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-08 14:36:18 +0900

    Add DummyDelayCommand module

---
+++ src/util/dummy_delay_command.c
@@ -0,0 +1,11 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "delay_command.h"
+
+static void Do(int time_in_milliseconds) {}
+
+static const DelayCommandMethodStruct kTheMethod = {
+    .Do = Do,
+};
+
+const DelayCommandMethod delayCommand = &kTheMethod;
