043cbd40e16c8454381baca4aa5169851e5dda07
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-20 09:25:11 +0900

    Add PulseMotorTransactionFactory interface

---
+++ v1/pulse_motor_transaction_factory_interface.h
@@ -0,0 +1,14 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_PULSE_MOTOR_TRANSACTION_FACTORY_INTERFACE_H_
+#define V1_PULSE_MOTOR_TRANSACTION_FACTORY_INTERFACE_H_
+
+#include "actuator_transaction_interface.h"
+#include "pulse_motor_interface.h"
+
+typedef struct {
+  ActuatorTransactionInterface (*MakePulseTransaction)(PulseMotorInterface pmi, int pulse);
+} PulseMotorTransactionFactoryInterfaceStruct;
+typedef const PulseMotorTransactionFactoryInterfaceStruct* PulseMotorTransactionFactoryInterface;
+
+#endif  // V1_PULSE_MOTOR_TRANSACTION_FACTORY_INTERFACE_H_
