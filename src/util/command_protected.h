445b2c71e93e01e3b0e17412035aafdbebf47165
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-21 12:10:13 +0900

    Add Command absract class

---
+++ src/util/command_protected.h
@@ -0,0 +1,12 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef SRC_UTIL_COMMAND_PROTECTED_H_
+#define SRC_UTIL_COMMAND_PROTECTED_H_
+
+#include "command.h"
+
+typedef struct CommandStruct {
+  CommandAbstractMethod impl;  //
+} CommandStruct;
+
+#endif  // SRC_UTIL_COMMAND_PROTECTED_H_
---
+++ v1/command.h
@@ -0,0 +1,17 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_COMMAND_H_
+#define V1_COMMAND_H_
+
+typedef struct CommandStruct* Command;
+typedef struct {
+  void (*Delete)(Command* self);  // Abstract method
+  void (*Do)(Command self);       // Abstract method
+  void (*Undo)(Command self);     // Abstract method
+  void (*Redo)(Command self);     // Abstract method
+} CommandAbstractMethodStruct;
+typedef const CommandAbstractMethodStruct* CommandAbstractMethod;
+
+extern const CommandAbstractMethod command;
+
+#endif  // V1_COMMAND_H_
