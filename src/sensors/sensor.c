dfe4865e26c86755923c83d24f80b1b6562c5b18
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-28 18:52:16 +0900

    Add Sensor interface

---
+++ src/sensors/sensor.c
@@ -0,0 +1,30 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "sensor.h"
+
+#include "sensor_private.h"
+
+static void Delete(Sensor* self) { (*self)->impl->Delete(self); }
+
+static Command SensorWatchCommand(Sensor self, const char* expected_state, Command notification_command) {
+  return self->impl->SensorWatchCommand(self, expected_state, notification_command);
+}
+
+static int GetId(Sensor self) { return self->impl->GetId(self); }
+
+static const char* GetTag(Sensor self) { return self->impl->GetTag(self); }
+
+static const char* GetState(Sensor self) { return self->impl->GetState(self); }
+
+static bool IsDefaultState(Sensor self) { return self->impl->IsDefaultState(self); }
+
+static const SensorInterfaceStruct kTheInterface = {
+    .Delete = Delete,
+    .SensorWatchCommand = SensorWatchCommand,
+    .GetId = GetId,
+    .GetTag = GetTag,
+    .GetState = GetState,
+    .IsDefaultState = IsDefaultState,
+};
+
+const SensorInterface sensor = &kTheInterface;
---
+++ v1/sensor.h
@@ -0,0 +1,23 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SENSOR_H_
+#define V1_SENSOR_H_
+
+#include <stdbool.h>
+
+#include "command.h"
+
+typedef struct SensorStruct* Sensor;
+typedef struct {
+  void (*Delete)(Sensor* self);
+  Command (*SensorWatchCommand)(Sensor self, const char* expected_state, Command notification_command);
+  int (*GetId)(Sensor self);
+  const char* (*GetTag)(Sensor self);
+  const char* (*GetState)(Sensor self);
+  bool (*IsDefaultState)(Sensor self);
+} SensorInterfaceStruct;
+typedef const SensorInterfaceStruct* SensorInterface;
+
+extern const SensorInterface sensor;
+
+#endif  // V1_SENSOR_H_
