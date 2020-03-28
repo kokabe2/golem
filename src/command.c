cc3a99d18c653b5d74c962d201089d300d085cda
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-28 15:54:25 +0900

    Add Command interface

---
+++ src/command.c
@@ -0,0 +1,15 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "command.h"
+
+#include "command_private.h"
+
+static void Delete(Command *self) { (*self)->impl->Delete(self); }
+
+static void Do(Command self) { self->impl->Do(self); }
+
+static const CommandInterfaceStruct kTheInterface = {
+    .Delete = Delete, .Do = Do,
+};
+
+const CommandInterface command = &kTheInterface;
--- v1/command_private.h
+++ v1/command_private.h
@@ -1,12 +1,12 @@
 ﻿// Copyright(c) 2020 Ken Okabe
 // This software is released under the MIT License, see LICENSE.
-#ifndef V1_COMMAND_PROTECTED_H_
-#define V1_COMMAND_PROTECTED_H_
+#ifndef V1_COMMAND_PRIVATE_H_
+#define V1_COMMAND_PRIVATE_H_
 
 #include "command.h"
 
 typedef struct CommandStruct {
-  CommandAbstractMethod impl;  //
+  CommandInterface impl;  //
 } CommandStruct;
 
-#endif  // V1_COMMAND_PROTECTED_H_
+#endif  // V1_COMMAND_PRIVATE_H_
