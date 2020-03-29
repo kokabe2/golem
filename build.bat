44c43b6fd489d4ccbf6414f8985bdff147f3f605
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 11:37:58 +0900

    Remove unnecessary Factory files

--- build.bat
+++ build.bat
@@ -10,14 +10,11 @@ set OUTPUT_DIR=bin\%COMPILER%@%PROJECT_VER%
 mkdir %OUTPUT_DIR% > NUL 2>&1
 %COMPILER% -output=obj -lang=c99 -nostuff -isa=rxv3 -debug -signed_char ^
            -include=v1/,external/ ^
-           src/sensors/microswitches/microswitch_factory.c ^
            src/sensors/microswitches/microswitch.c ^
            src/sensors/microswitches/null_microswitch.c ^
            src/sensors/photosensors/null_photosensor.c ^
-           src/sensors/photosensors/photosensor_factory.c ^
            src/sensors/photosensors/photosensor.c ^
            src/sensors/null_sensor.c ^
-           src/sensors/sensor_factory.c ^
            src/sensors/sensor.c ^
            src/util/active_object_engine_task.c ^
            src/util/active_object_engine.c ^
