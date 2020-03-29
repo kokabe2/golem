4651fd177ba62dcfb0ed0e3a55b16603ad69419c
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 13:29:53 +0900

    Add Display interface

---
+++ src/displays/display.c
@@ -0,0 +1,22 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "display.h"
+
+#include "display_private.h"
+
+static void Delete(Display* self) { (*self)->impl->Delete(self); }
+
+static int Id(Display self) { return self->impl->Id(self); }
+
+static const char* Tag(Display self) { return self->impl->Tag(self); }
+
+static void Reset(Display self) { self->impl->Reset(self); }
+
+static const DisplayInterfaceStruct kTheInterface = {
+    .Delete = Delete,
+    .Id = Id,
+    .Tag = Tag,
+    .Reset = Reset,
+};
+
+const DisplayInterface display = &kTheInterface;
---
+++ v1/display.h
@@ -0,0 +1,17 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_DISPLAY_H_
+#define V1_DISPLAY_H_
+
+typedef struct DisplayStruct* Display;
+typedef struct {
+  void (*Delete)(Display* self);
+  int (*Id)(Display self);
+  const char* (*Tag)(Display self);
+  void (*Reset)(Display self);
+} DisplayInterfaceStruct;
+typedef const DisplayInterfaceStruct* DisplayInterface;
+
+extern const DisplayInterface display;
+
+#endif  // V1_DISPLAY_H_
