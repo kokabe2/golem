03f6951e25d995e57706a2fc9ffaf3d40123c005
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 10:32:13 +0900

    Add SensorFactory interface

---
+++ v1/sensor_factory_private.h
@@ -0,0 +1,12 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SENSOR_FACTORY_PRIVATE_H_
+#define V1_SENSOR_FACTORY_PRIVATE_H_
+
+#include "sensor_factory.h"
+
+typedef struct SensorFactoryStruct {
+  SensorFactoryInterface impl;  //
+} SensorFactoryStruct;
+
+#endif  // V1_SENSOR_FACTORY_PRIVATE_H_
