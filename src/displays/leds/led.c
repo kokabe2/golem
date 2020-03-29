029333844843c9eaa3b414d2bd71f9fac823aa2e
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 13:40:03 +0900

    Add Led interface

---
+++ src/displays/leds/led.c
@@ -0,0 +1,31 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "led.h"
+
+#include "led_private.h"
+
+static void Delete(Led* self) { (*self)->impl->Delete(self); }
+
+static int Id(Led self) { return self->impl->Id(self); }
+
+static const char* Tag(Led self) { return self->impl->Tag(self); }
+
+static void TurnOff(Led self) { self->impl->TurnOff(self); }
+
+static void TurnOn(Led self) { self->impl->TurnOn(self); }
+
+static bool IsOff(Led self) { return self->impl->IsOff(self); }
+
+static bool IsOn(Led self) { return self->impl->IsOn(self); }
+
+static const LedInterfaceStruct kTheInterface = {
+    .Delete = Delete,
+    .Id = Id,
+    .Tag = Tag,
+    .TurnOff = TurnOff,
+    .TurnOn = TurnOn,
+    .IsOff = IsOff,
+    .IsOn = IsOn,
+};
+
+const LedInterface led = &kTheInterface;
---
+++ v1/led.h
@@ -0,0 +1,22 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_LED_H_
+#define V1_LED_H_
+
+#include <stdbool.h>
+
+typedef struct LedStruct* Led;
+typedef struct {
+  void (*Delete)(Led* self);
+  int (*Id)(Led self);
+  const char* (*Tag)(Led self);
+  void (*TurnOff)(Led self);
+  void (*TurnOn)(Led self);
+  bool (*IsOff)(Led self);
+  bool (*IsOn)(Led self);
+} LedInterfaceStruct;
+typedef const LedInterfaceStruct* LedInterface;
+
+extern const LedInterface led;
+
+#endif  // V1_LED_H_
