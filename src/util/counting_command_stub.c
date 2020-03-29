da7c30785b88f42898d08341ae2653a9f8dc9cc3
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 22:41:37 +0900

    Add CountingCommandStub class

---
+++ src/util/counting_command_stub.c
@@ -0,0 +1,41 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "counting_command_stub.h"
+
+#include "bleu/v1/heap.h"
+#include "command_private.h"
+
+typedef struct {
+  CommandStruct base;
+  int count;
+} CountingCommandStubStruct, *CountingCommandStub;
+
+static void Delete(Command *self) { heap->Delete((void **)self); }
+
+static void Do(Command base) {
+  CountingCommandStub self = (CountingCommandStub)base;
+  ++self->count;
+}
+
+static const CommandInterfaceStruct kTheInterface = {
+    .Delete = Delete,
+    .Do = Do,
+};
+
+static Command New(void) {
+  CountingCommandStub self = (CountingCommandStub)heap->New(sizeof(CountingCommandStubStruct));
+  self->base.impl = &kTheInterface;
+  return (Command)self;
+}
+
+static int Count(Command base) {
+  CountingCommandStub self = (CountingCommandStub)base;
+  return self->count;
+}
+
+static const CountingCommandStubMethodStruct kTheMethod = {
+    .New = New,
+    .Count = Count,
+};
+
+const CountingCommandStubMethod countingCommandStub = &kTheMethod;
---
+++ src/util/counting_command_stub_test.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "counting_command_stub.h"
+}
+
+class CountingCommandStubTest : public ::testing::Test {
+ protected:
+  Command c;
+
+  virtual void SetUp() { c = countingCommandStub->New(); }
+
+  virtual void TearDown() { command->Delete(&c); }
+};
+
+TEST_F(CountingCommandStubTest, CountWhenHaveNeverDo) { EXPECT_EQ(0, countingCommandStub->Count(c)); }
+
+TEST_F(CountingCommandStubTest, Count) {
+  command->Do(c);
+  command->Do(c);
+  command->Do(c);
+
+  EXPECT_EQ(3, countingCommandStub->Count(c));
+}
