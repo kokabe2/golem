ebfe64d0769a4fa5dee3738ed31ef903d56de603
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 14:19:55 +0900

    Remove unused files

--- src/actuators/null_actuator_transaction_test.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Copyright(c) 2020 Ken Okabe
-// This software is released under the MIT License, see LICENSE.
-#include "gtest/gtest.h"
-
-extern "C" {
-#include "null_actuator_transaction.h"
-}
-
-TEST(NullActuatorTransactionTest, ExecuteReturnsFalse) { EXPECT_FALSE(nullActuatorTransaction->Execute()); }
