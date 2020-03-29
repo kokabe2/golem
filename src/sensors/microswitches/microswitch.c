86936012139e3cffccd5f4580ba3e8c4a59e9a81
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 10:38:06 +0900

    Add Microswitch interface

---
+++ src/sensors/microswitches/microswitch.c
@@ -0,0 +1,33 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "microswitch.h"
+
+#include "microswitch_private.h"
+
+static void Delete(Microswitch* self) { (*self)->impl->Delete(self); }
+
+static Command SensorWatchCommand(Microswitch self, const char* expected_state, Command notification_command) {
+  return self->impl->SensorWatchCommand(self, expected_state, notification_command);
+}
+
+static int Id(Microswitch self) { return self->impl->Id(self); }
+
+static const char* Tag(Microswitch self) { return self->impl->Tag(self); }
+
+static const char* State(Microswitch self) { return self->impl->State(self); }
+
+static bool IsOff(Microswitch self) { return self->impl->IsOff(self); }
+
+static bool IsOn(Microswitch self) { return self->impl->IsOn(self); }
+
+static const MicroswitchInterfaceStruct kTheInterface = {
+    .Delete = Delete,
+    .SensorWatchCommand = SensorWatchCommand,
+    .Id = Id,
+    .Tag = Tag,
+    .State = State,
+    .IsOff = IsOff,
+    .IsOn = IsOn,
+};
+
+const MicroswitchInterface microswitch = &kTheInterface;
---
+++ v1/microswitch.h
@@ -0,0 +1,22 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_MICROSWITCH_H_
+#define V1_MICROSWITCH_H_
+
+#include <stdbool.h>
+
+#include "command.h"
+
+typedef struct MicroswitchStruct* Microswitch;
+typedef struct {
+  void (*Delete)(Microswitch* self);
+  Command (*SensorWatchCommand)(Microswitch self, const char* expected_state, Command notification_command);
+  int (*Id)(Microswitch self);
+  const char* (*Tag)(Microswitch self);
+  const char* (*State)(Microswitch self);
+  bool (*IsOff)(Microswitch self);
+  bool (*IsOn)(Microswitch self);
+} MicroswitchInterfaceStruct;
+typedef const MicroswitchInterfaceStruct* MicroswitchInterface;
+
+#endif  // V1_MICROSWITCH_H_
