12f7c5b7b50ac0103e61dd2371f535c0606df182
Ken Okabe <ken.okabe.1.168@gmail.com>
2020-03-29 14:05:07 +0900

    Add SevenSegmentDisplay interface

---
+++ src/displays/seven_segment_displays/seven_segment_display.c
@@ -0,0 +1,28 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#include "seven_segment_display.h"
+
+#include "seven_segment_display_private.h"
+
+static void Delete(SevenSegmentDisplay* self) { (*self)->impl->Delete(self); }
+
+static int Id(SevenSegmentDisplay self) { return self->impl->Id(self); }
+
+static const char* Tag(SevenSegmentDisplay self) { return self->impl->Tag(self); }
+
+static void Clear(SevenSegmentDisplay self) { self->impl->Clear(self); }
+
+static uint8_t Get(SevenSegmentDisplay self) { return self->impl->Get(self); }
+
+static void Set(SevenSegmentDisplay self, uint8_t encoding) { self->impl->Set(self, encoding); }
+
+static const SevenSegmentDisplayInterfaceStruct kTheInterface = {
+    .Delete = Delete,
+    .Id = Id,
+    .Tag = Tag,
+    .Clear = Clear,
+    .Get = Get,
+    .Set = Set,
+};
+
+const SevenSegmentDisplayInterface sevenSegmentDisplay = &kTheInterface;
---
+++ v1/seven_segment_display_private.h
@@ -0,0 +1,14 @@
+﻿// Copyright(c) 2020 Ken Okabe
+// This software is released under the MIT License, see LICENSE.
+#ifndef V1_SEVEN_SEGMENT_DISPLAY_PRIVATE_H_
+#define V1_SEVEN_SEGMENT_DISPLAY_PRIVATE_H_
+
+#include "seven_segment_display.h"
+
+typedef struct SevenSegmentDisplayStruct {
+  SevenSegmentDisplayInterface impl;
+  int id;
+  const char *tag;
+} SevenSegmentDisplayStruct;
+
+#endif  // V1_SEVEN_SEGMENT_DISPLAY_PRIVATE_H_
